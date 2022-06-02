57f4881ad3863c312c0efde488975a7a42a22ea7
Tharanga Mahavila <tharangamahavila@gmail.com>
2022-06-02 22:25:41 +0530

    completed template 1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -39,7 +39,16 @@ const submitForm = (data) =>{
 
 }
 
-const FormTest = ({setName,setBirthDay,setAddress}) => {
+const FormTest = ({setName,setBirthDay,
+                    setMail,setCourse,setInstitute,
+                    setCourseDuration,setGraduation,
+                    setEduDescription,setCompany,setRole,
+                    setJobDuration,setJobDescription,
+                    setProfSkill,setProfSkillRate,
+                    setSoftSkill,setSoftSkillRate,
+                    setCodeSkill,setCodeSkillRate,
+                    setInterest,setIntDescription,
+                    setAddress,setPhone}) => {
 
     const {register,handleSubmit,formState: { errors }} = useForm({
         resolver: yupResolver(schema)
@@ -53,49 +62,99 @@ const FormTest = ({setName,setBirthDay,setAddress}) => {
                 <form>
                 <Grid container spacing={1}>
                     <Typography gutterBottom variant="h5">Personal Details</Typography>
+
                     <Grid xs={12} sm={12} item>
                         <TextField label='Full Name' placeholder="Enter Full Name" fullWidth variant="outlined" size="small" required onChange={(e)=>setName(e.target.value)}/>
-                        {/* <input type="text" placeholder="Full Name" onChange={(e)=>setName(e.target.value)} /> */}
                     </Grid>
+
                     <Grid  xs={12} sm={12} item>
                         <TextField label='Date Of Birth' placeholder="Enter Your Birthday" fullWidth variant="outlined" size="small" required onChange={(e)=>setBirthDay(e.target.value)}/>
-                        {/* <input type="text" placeholder="Full Name" onChange={(e)=>setName(e.target.value)} /> */}
                     </Grid>
+
                     <Grid  xs={12} sm={12} item>
-                        <TextField type={'email'} label='Email' placeholder="Enter Your Email" fullWidth variant="outlined" size="small" required onChange={(e)=>setBirthDay(e.target.value)}/>
-                        {/* <input type="text" placeholder="Full Name" onChange={(e)=>setName(e.target.value)} /> */}
+                        <TextField type={'email'} label='Email' placeholder="Enter Your Email" fullWidth variant="outlined" size="small" required onChange={(e)=>setMail(e.target.value)}/>
                     </Grid>
+
                     <Typography gutterBottom variant="h5">Education</Typography>
+
                     <Grid  xs={12} sm={12} item>
-                        <TextField label='Course' placeholder="Enter Your Course"  fullWidth variant="outlined" size="small" required onChange={(e)=>setBirthDay(e.target.value)}/>
-                        <TextField label='Institute' placeholder="Enter Your Institute" fullWidth variant="outlined" size="small"  required onChange={(e)=>setBirthDay(e.target.value)}/>
-                        <TextField label='Description'placeholder="Enter course Description" fullWidth  variant="outlined" size="small" onChange={(e)=>setBirthDay(e.target.value)}/>
+                        <TextField label='Course' placeholder="Enter Your Course"  fullWidth variant="outlined" size="small" required onChange={(e)=>setCourse(e.target.value)}/>
                     </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                        <TextField type={'number'} min='1' label='Course Duration' placeholder="Enter Your Course Duration"  fullWidth variant="outlined" size="small" required onChange={(e)=>setCourseDuration(e.target.value)}/>
+                    </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                        <TextField label='Institute' placeholder="Enter Your Institute" fullWidth variant="outlined" size="small"  required onChange={(e)=>setInstitute(e.target.value)}/>
+                    </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                        <TextField label='Graduation' placeholder="Enter Your Graduation Date"  fullWidth variant="outlined" size="small" required onChange={(e)=>setGraduation(e.target.value)}/>
+                    </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                        <TextField label='Description'placeholder="Enter course Description" fullWidth  variant="outlined" size="small" onChange={(e)=>setEduDescription(e.target.value)}/>
+                    </Grid>
+
                     <Typography gutterBottom variant="h5">Work Experience</Typography>
+
                     <Grid  xs={12} sm={12} item>
-                        <TextField label='Company' placeholder="Enter Your company" fullWidth variant="outlined" size="small" onChange={(e)=>setBirthDay(e.target.value)}/>
-                        <TextField label='Job Role' placeholder="Enter Your Job Role" fullWidth variant="outlined" size="small" onChange={(e)=>setBirthDay(e.target.value)}/>
-                        <TextField label='Description' placeholder="Enter Job Description" fullWidth variant="outlined" size="small" onChange={(e)=>setBirthDay(e.target.value)}/>
+                        <TextField label='Company' placeholder="Enter Your company" fullWidth variant="outlined" size="small" onChange={(e)=>setCompany(e.target.value)}/>                       
                     </Grid>
+                    <Grid xs={12} sm={12} item>
+                         <TextField label='Job Role' placeholder="Enter Your Job Role" fullWidth variant="outlined" size="small" onChange={(e)=>setRole(e.target.value)}/>
+                    </Grid>
+                    <Grid xs={12} sm={12} item>
+                        <TextField label='Job Duration' placeholder="Enter Job Duration" fullWidth variant="outlined" size="small" onChange={(e)=>setJobDuration(e.target.value)}/>
+                    </Grid>
+                    <Grid xs={12} sm={12} item>
+                        <TextField label='Description' placeholder="Enter Job Description" fullWidth variant="outlined" size="small" onChange={(e)=>setJobDescription(e.target.value)}/>
+                    </Grid>
+
                     <Typography gutterBottom variant="h5">Skills</Typography>
+
                     <Grid  xs={12} sm={12} item>
-                        <TextField label='Professional skills' placeholder="Enter Your Professional skills" fullWidth variant="outlined" size="small" onChange={(e)=>setBirthDay(e.target.value)}/>
-                        <TextField label='Software Skills' placeholder="Enter Your Software Skills" fullWidth  variant="outlined" size="small" onChange={(e)=>setBirthDay(e.target.value)}/>
-                        <TextField label='Code Skills' placeholder="Enter Your Code Skills" fullWidth variant="outlined" size="small" onChange={(e)=>setBirthDay(e.target.value)}/>
+                        <TextField label='Professional skills' placeholder="Enter Your Professional skills" fullWidth variant="outlined" size="small" onChange={(e)=>setProfSkill(e.target.value)}/>
                     </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                        <TextField type={'number'} label='Professional skills Rate' placeholder="Enter Your Professional skills Rate" fullWidth variant="outlined" size="small" onChange={(e)=>setProfSkillRate(e.target.value)}/>
+                    </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                        <TextField label='Software Skills' placeholder="Enter Your Software Skills" fullWidth  variant="outlined" size="small" onChange={(e)=>setSoftSkill(e.target.value)}/>
+                    </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                        <TextField type={'number'} label='Software Skills Rate' placeholder="Enter Your Software Skills Rate" fullWidth  variant="outlined" size="small" onChange={(e)=>setSoftSkillRate(e.target.value)}/>
+                    </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                         <TextField label='Code Skills' placeholder="Enter Your Code Skills" fullWidth variant="outlined" size="small" onChange={(e)=>setCodeSkill(e.target.value)}/>
+                    </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                        <TextField type={'number'} label='Code Skills Rate' placeholder="Enter Your Code Skills Rate" fullWidth variant="outlined" size="small" onChange={(e)=>setCodeSkillRate(e.target.value)}/>
+                    </Grid>
+
                     <Typography gutterBottom variant="h5">Interests</Typography>
+
                     <Grid  xs={12} sm={12} item>
-                        <TextField label='Interests' placeholder="Enter Your Interests" fullWidth variant="outlined" size="small" onChange={(e)=>setBirthDay(e.target.value)}/>
-                        <TextField label='Description' placeholder="Enter about Interest" fullWidth variant="outlined" size="small" onChange={(e)=>setBirthDay(e.target.value)}/>
+                        <TextField label='Interests' placeholder="Enter Your Interests" fullWidth variant="outlined" size="small" onChange={(e)=>setInterest(e.target.value)}/>
                     </Grid>
+
+                    <Grid xs={12} sm={12} item>
+                        <TextField label='Description' placeholder="Enter about Interest" fullWidth variant="outlined" size="small" onChange={(e)=>setIntDescription(e.target.value)}/>
+                    </Grid>
+
                     <Typography gutterBottom variant="h5">Contact</Typography>
+
                     <Grid  xs={12} sm={12} item>
-                        <TextField label='Address' multiline rows={'2'} placeholder="Enter Your Adress" fullWidth variant="outlined" size="small" required onChange={(e)=>setBirthDay(e.target.value)}/>
-                        {/* <input type="text" placeholder="Full Name" onChange={(e)=>setName(e.target.value)} /> */}
+                        <TextField label='Address' multiline rows={'2'} placeholder="Enter Your Adress" fullWidth variant="outlined" size="small" required onChange={(e)=>setAddress(e.target.value)}/>
                     </Grid>
                     <Grid  xs={12} sm={12} item>
-                        <TextField type={'number'} label='Phone' placeholder="Enter Your Phone Number" fullWidth variant="outlined" size="small"required onChange={(e)=>setBirthDay(e.target.value)}/>
-                        {/* <input type="text" placeholder="Full Name" onChange={(e)=>setName(e.target.value)} /> */}
+                        <TextField type={'number'} label='Phone' placeholder="Enter Your Phone Number" fullWidth variant="outlined" size="small"required onChange={(e)=>setPhone(e.target.value)}/>
                     </Grid>
                     <Grid  xs={12} sm={12} item>
                         <Button type="Submit" variant="contained" color="primary">Submit</Button>
@@ -104,11 +163,6 @@ const FormTest = ({setName,setBirthDay,setAddress}) => {
                 </form>
             </CardContent>
         </Card>   
-        // <Form>
-        //     <input type="text" name="fullName" placeholder="Full Name" onChange={(e)=> setName(e.target.value)}/>
-        //     <input type="text" name="birthDay" placeholder="Birth Day" onChange={(e) => setBirthDay(e.target.value)}/>
-        //     <input type="text" name="address" placeholder="Address" onChange={(e)=> setAddress(e.target.value)}/>
-        // </Form>
      );
 }
  
--- src/components/Template1/template1.jsx
+++ src/components/Template1/template1.jsx
@@ -12,17 +12,112 @@ const TemplateOne = ({dummyData=false}) => {
     const [name, setName] = useState('');
     const [birthDay, setBirthDay] = useState('');
     const [address, setAddress] = useState('');
+    const [mail, setMail] = useState('');
+    const [course,setCourse] = useState('');
+    const [institute, setInstitute] = useState('');
+    const [CourseDuration,setCourseDuration] = useState('');
+    const [graduation,setGraduation] = useState('');
+    const [eduDescription,setEduDescription] = useState('')
+    const [company,setCompany] = useState('');
+    const [jobRole,setRole] = useState('');
+    const [jobDuration,setJobDuration] = useState('');
+    const [jobDescription,setJobDescription] = useState('');
+    const [profSkill,setProfSkill] = useState('');
+    const [profSkillRate,setProfSkillRate] = useState('');
+    const [softSkill,setSoftSkill] = useState('');
+    const [softSkillRate,setSoftSkillRate] = useState('')
+    const [codeSkill,setCodeSkill] = useState('')
+    const [codeSkillRate,setCodeSkillRate] = useState('')
+    const [interest,setInterest] = useState('');
+    const [intDescription,setIntDescription] = useState('')
+    const [phone,setPhone] = useState('')
 
     const bindName = (name) => {
         setName(name);
     }
 
     const bindBirthDay =(birthDay) => {
-        setBirthDay(birthDay)
+        setBirthDay(birthDay);
     }
 
-    const bindAdress = (address) =>{
-        setAddress(address)
+    const bindAddress = (address) =>{
+        setAddress(address);
+    }
+
+    const bindEmail = (mail)=>{
+        setMail(mail);
+    }
+
+    const bindCourse = (course) =>{
+        setCourse(course);
+    }
+
+    const bindInstitute = (institute) =>{
+        setInstitute(institute);
+    }
+
+    const bindCourseDuration = (CourseDuration) =>{
+        setCourseDuration(CourseDuration);
+    }
+
+    const bindGraduation = (graduation)=>{
+        setGraduation(graduation);
+    }
+
+    const bindEduDescription = (eduDescription)=>{
+        setEduDescription(eduDescription)
+    }
+
+    const bindCompany = (company) =>{
+        setCompany(company);
+    }
+
+    const bindRole=(jobRole)=>{
+        setRole(jobRole)
+    }
+
+    const bindJobDuration =(jobDuration)=>{
+        setJobDuration(jobDuration);
+    }
+
+    const bindJobDescription = (jobDescription)=>{
+        setJobDescription(jobDescription)
+    }
+
+    const bindProfSkill = (profSkill)=> {
+        setProfSkill(profSkill);
+    }
+
+    const bindProfSkillRate = (profSkillRate)=>{
+        setProfSkillRate(profSkillRate);
+    }
+
+    const bindSoftSkill = (softSkill)=>{
+        setSoftSkill(softSkill);
+    }
+
+    const bindSoftSkillRate = (softSkillRate)=>{
+        setSoftSkillRate(softSkillRate);
+    }
+
+    const bindCodeSkill = (codeSkill)=>{
+        setCodeSkill(codeSkill);
+    }
+
+    const bindCodeSkillRate = (codeSkillRate)=>{
+        setCodeSkillRate(codeSkillRate);
+    }
+
+    const bindInterest = (interest)=>{
+        setInterest(interest);
+    }
+
+    const bindIntDescription = (intDescription)=>{
+        setIntDescription(intDescription);
+    }
+
+    const bindPhone = (phone)=>{
+        setPhone(phone);
     }
     return ( 
         <div className="background">
@@ -65,7 +160,7 @@ const TemplateOne = ({dummyData=false}) => {
                                 </div>
                                 <div className="line-content">
                                     <h3 className="section-item-title-1">Email</h3>
-                                    <p>john@dotrex.co</p>
+                                    <p>{dummyData? 'john@dotrex.co': mail}</p>
                                 </div>
                                 <div className="line-content">
                                     <h3 class="section-item-title-1">Find Me On</h3>
@@ -87,10 +182,10 @@ const TemplateOne = ({dummyData=false}) => {
                                     <h2 className="section-title">Education</h2>
                                 </div>
                                 <div className="line-content">
-                                <h3 className="section-item-title-1">GRAPHIC DESIGN</h3>
-                                <h4 className="graduation-time"><FaUniversity/> St. Patrick University (2 Years Course) - <span class="graduation-date">Graduation May 2013</span></h4>
+                                <h3 className="section-item-title-1">{dummyData?'GRAPHIC DESIGN': course}</h3>
+                                <h4 className="graduation-time"><FaUniversity/>{dummyData?'St. Patrick University': institute+ ('-')} {dummyData?'(2 Years Course) -':CourseDuration+' Years Course'} <span class="graduation-date">{dummyData?'Graduation May 2013': graduation}</span></h4>
                                 <div className="graduation-description">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <p> {dummyData? 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.': eduDescription}</p>
                                 </div>
                                 </div> 
                                 <div className="line-content">
@@ -115,10 +210,10 @@ const TemplateOne = ({dummyData=false}) => {
                                 <h2 className="section-title">Work Experience</h2> 
                                 </div>
                                 <div className="line-content">
-                                <h3 className="section-item-title-1">Black Tie Corp</h3>
-                                <h4 className="job"><FaFlag/> Web Designer - <span class="job-date">June 2012 - Current</span></h4>
+                                <h3 className="section-item-title-1">{dummyData? 'Black Tie Corp' : company}</h3>
+                                <h4 className="job"><FaFlag/>{dummyData?'Web Designer -': jobRole+ ' -'} <span class="job-date"> {dummyData? 'June 2012 - Current': jobDuration}</span></h4>
                                 <div className="job-description">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                    <p>{dummyData? 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.': jobDescription}</p>
                                 </div>
                                 </div>
                                 <div className="line-content">
@@ -147,9 +242,9 @@ const TemplateOne = ({dummyData=false}) => {
                                 <ul className="skills-list">
                                     <li>
                                         <div className="progress">
-                                        <span className="progress-type">Comunication</span>
+                                        <span className="progress-type"> {dummyData? 'Comunication' :profSkill}</span>
                                         {/* <span class="progress-completed">70%</span> */}
-                                            <ProgressBar completed="70"/>
+                                            <ProgressBar completed={dummyData? "70": profSkillRate}/>
                                         </div>
                                     </li>
                                     <li>
@@ -173,9 +268,9 @@ const TemplateOne = ({dummyData=false}) => {
                                 <ul className="skills-list">
                                     <li>
                                         <div className="progress">
-                                        <span className="progress-type">Adobe Photoshop</span>
+                                        <span className="progress-type">{dummyData? 'Adobe Photoshop': softSkill}</span>
                                         {/* <span class="progress-completed">70%</span> */}
-                                            <ProgressBar completed="90"/>
+                                            <ProgressBar completed= {dummyData?"90": softSkillRate}/>
                                         </div>
                                     </li>
                                     <li>
@@ -199,9 +294,9 @@ const TemplateOne = ({dummyData=false}) => {
                                 <ul className="skills-list">
                                     <li>
                                         <div className="progress">
-                                        <span className="progress-type">HTML5 / CSS3</span>
+                                        <span className="progress-type">{dummyData? 'HTML5 / CSS3':codeSkill}</span>
                                         {/* <span class="progress-completed">70%</span> */}
-                                            <ProgressBar completed="90"/>
+                                            <ProgressBar completed={dummyData?"90": codeSkillRate}/>
                                         </div>
                                     </li>
                                     <li>
@@ -228,9 +323,9 @@ const TemplateOne = ({dummyData=false}) => {
                                 <h2 className="section-title">Interests</h2> 
                                 </div>
                                 <div className="line-content">
-                                <h3 className="section-item-title-1">Art</h3>
+                                <h3 className="section-item-title-1">{dummyData? 'Art': interest}</h3>
                                 <div className="job-description">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                    <p>{dummyData? 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.': intDescription}</p>
                                 </div>
                                 </div>
                                 <div className="line-content">
@@ -257,9 +352,9 @@ const TemplateOne = ({dummyData=false}) => {
                                     <h4 className="contact-subtitle-1">Address</h4>
                                     <p>{dummyData? '451 Lorem Ipsum, Austin - Texas - U.S.A' : address}</p>
                                     <h4 className="contact-subtitle-1">Phone</h4>
-                                    <p>+61 3 8376 6284</p>
+                                    <p> {dummyData?'+61 3 8376 6284' : phone}</p>
                                     <h4 className="contact-subtitle-1">Mail</h4>
-                                    <p>john.Rex@dotrex.com</p>
+                                    <p>{dummyData?'john.Rex@dotrex.com':mail}</p>
                                     </div>
                                 </div>
                             </div>
@@ -269,7 +364,26 @@ const TemplateOne = ({dummyData=false}) => {
                         <div>
                             <Form setName={(e)=> bindName(e)} 
                             setBirthDay={(e)=> bindBirthDay(e)}
-                            setAddress = {(e) => bindAdress(e)}
+                            setMail = {(e)=> bindEmail(e)}
+                            setCourse = {(e)=> bindCourse(e)}
+                            setInstitute ={(e)=> bindInstitute(e)}
+                            setCourseDuration = {(e)=> bindCourseDuration(e)}
+                            setGraduation = {(e)=> bindGraduation(e)}
+                            setEduDescription = {(e)=> bindEduDescription(e)}
+                            setCompany = {(e)=> bindCompany(e)}
+                            setRole = {(e)=> bindRole(e)}
+                            setJobDuration = {(e)=> bindJobDuration(e)}
+                            setJobDescription = {(e)=> bindJobDescription(e)}
+                            setProfSkill = {(e)=> bindProfSkill(e)}
+                            setProfSkillRate = {(e)=> bindProfSkillRate(e)}
+                            setSoftSkill = {(e)=> bindSoftSkill(e)}
+                            setSoftSkillRate = {(e)=> bindSoftSkillRate(e)}
+                            setCodeSkill = {(e)=>bindCodeSkill(e)}
+                            setCodeSkillRate = {(e)=> bindCodeSkillRate(e)}
+                            setInterest = {(e)=> bindInterest(e)}
+                            setIntDescription = {(e)=> bindIntDescription(e)}
+                            setAddress = {(e) => bindAddress(e)}
+                            setPhone = {(e)=>bindPhone(e)}
                             />
                         </div>
                     </div>
